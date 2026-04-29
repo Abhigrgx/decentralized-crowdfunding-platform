7179106e5f2e2725e92e3cf53e3091345ac9d6d7
Abhishek Gurung <u2649201@uel.ac.uk>
2026-04-29 02:57:53 +0000

    frontend: refresh connect wallet call-to-action

--- frontend/src/components/ConnectWallet.jsx
+++ frontend/src/components/ConnectWallet.jsx
@@ -1,14 +1,14 @@
-export default function ConnectWallet(props) {
-    return (
-        <div className='connectWallet'>
-            <div className='typingContainer'>
-                <div className='typing'>DeFindstarter</div>
-            </div>
-            <div className="walletButtonContainer">
-                <button className='walletButton' onClick={props.connectMetamask}>
-                    Connect to Metamask
-                </button>
-            </div>
-        </div>
-    );
+export default function ConnectWallet({ connectMetamask }) {
+  return (
+    <div className="connectWallet">
+      <div className="typingContainer">
+        <div className="typing">Welcome to DeFindStarter</div>
+      </div>
+      <div className="walletButtonContainer">
+        <button className="walletButton" onClick={connectMetamask}>
+          <span>Connect Wallet</span>
+        </button>
+      </div>
+    </div>
+  );
 }
