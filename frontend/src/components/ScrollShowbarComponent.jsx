13a765b915ec2ec1a6f0074fb2e5d7c9ddec18a4
Abhishek Gurung <u2649201@uel.ac.uk>
2026-04-29 02:58:05 +0000

    frontend: streamline scroll showcase card rendering

--- frontend/src/components/ScrollShowbarComponent.jsx
+++ frontend/src/components/ScrollShowbarComponent.jsx
@@ -3,64 +3,40 @@ import { BsArrowLeftCircle, BsArrowRightCircle } from "react-icons/bs";
 import dummyPic from "../assets/pg1.jpg";
 import { toPublicGatewayUrl } from "../utils/ipfs";
 
-export default function ScrollShowbarComponent(props) {
+export default function ScrollShowbarComponent({ recentUploads, heading, emptyMessage }) {
   const scroll = (val) => {
-    document.getElementsByClassName("recentUploadsContainer")[0].scrollLeft +=
-      val;
-  };
-  const renderCards = () => {
-    return props.recentUploads.map((project, index) => {
-      return (
-        <div className="projectCard" key={index}>
-          <Link to="/project" state={{ index: project.index }}>
-            <div
-              className="cardImg"
-              style={{
-                backgroundImage: project.cid
-                  ? `url(${toPublicGatewayUrl(project.cid)})`
-                  : dummyPic,
-              }}
-            ></div>
-          </Link>
-          <div className="cardDetail">
-            <div className="cardTitle">
-              <Link to="/project" state={{ index: project.index }}>
-                {project.projectName}
-              </Link>
-            </div>
-            <div className="cardDesc">{project.projectDescription}</div>
-            <div className="cardAuthor">{"By " + project.creatorName}</div>
-          </div>
-        </div>
-      );
-    });
+    document.getElementsByClassName("recentUploadsContainer")[0].scrollLeft += val;
   };
   return (
     <div className="recentUploads">
       <div className="recentUploadsHeader">
-        <div className="recentUploadsHeading">{props.heading}</div>
-        {props.recentUploads.length ? (
+        <div className="recentUploadsHeading">{heading}</div>
+        {recentUploads.length > 0 && (
           <div className="scrollButtons">
-            <BsArrowLeftCircle
-              className="scrollNavBtn"
-              onClick={() => scroll(-300)}
-            />
-            <BsArrowRightCircle
-              className="scrollNavBtn"
-              onClick={() => scroll(300)}
-            />
+            <BsArrowLeftCircle className="scrollNavBtn" onClick={() => scroll(-300)} />
+            <BsArrowRightCircle className="scrollNavBtn" onClick={() => scroll(300)} />
           </div>
-        ) : (
-          ""
         )}
       </div>
-
       <div className="recentUploadsContainer">
-        {props.recentUploads.length ? (
-          renderCards()
-        ) : (
-          <div className="noProjects">{props.emptyMessage}</div>
-        )}
+        {recentUploads.length > 0 ? recentUploads.map((project, index) => (
+          <div className="projectCard" key={index}>
+            <Link to="/project" state={{ index: project.index }}>
+              <div className="cardImgWrapper">
+                <div className="cardImg" style={{
+                  backgroundImage: project.cid ? `url(${toPublicGatewayUrl(project.cid)})` : `url(${dummyPic})`
+                }} />
+              </div>
+            </Link>
+            <div className="cardDetail">
+              <div className="cardTitle">
+                <Link to="/project" state={{ index: project.index }}>{project.projectName}</Link>
+              </div>
+              <div className="cardDesc">{project.projectDescription}</div>
+              <div className="cardAuthor">{"By " + project.creatorName}</div>
+            </div>
+          </div>
+        )) : <div className="noProjects">{emptyMessage}</div>}
       </div>
     </div>
   );
