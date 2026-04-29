4cca046e84d83f6e55811224758ea6cb004964e1
Abhishek Gurung <u2649201@uel.ac.uk>
2026-04-29 02:58:28 +0000

    frontend: polish footer socials and mobile navbar responsiveness

--- frontend/src/components/FooterComponent.jsx
+++ frontend/src/components/FooterComponent.jsx
@@ -1,43 +1,28 @@
-import { FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
+import { FaTwitter, FaGithub, FaInstagram } from "react-icons/fa";
 
-export default function FooterComponent(props) {
+export default function FooterComponent() {
   return (
     <div className="footer">
       <div className="footerContainer">
-        <div className="trademarkSection">DeFindstarter © 2022</div>
+        <div className="trademarkSection">
+          <span>© {new Date().getFullYear()} DeFindStarter</span>
+        </div>
         <div className="externalLinks">
-          <div className="linkWrapper">
-            <div className="icon">
-              <a
-                href="https://twitter.com/crypto_gaijin"
-                className="twitter-icon"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <FaTwitter />
+          <div className="icon">
+            <a href="https://twitter.com" className="twitter-icon" target="_blank" rel="noreferrer">
+              <FaTwitter />
+            </a>
+          </div>
+          <div className="icon">
+            <a href="https://github.com/SayanKar" className="github-icon" target="_blank" rel="noreferrer">
+              <FaGithub />
+            </a>
+          </div>
+          <div className="icon">
+            <div className="insta-icon-wrapper">
+              <a href="https://www.instagram.com/sayan.kar2000/" className="insta-icon" target="_blank" rel="noreferrer">
+                <FaInstagram />
               </a>
-            </div>
-            <div className="icon">
-              <a
-                href="https://github.com/hyp3r5pace"
-                className="github-icon"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <FaGithub />
-              </a>
-            </div>
-            <div className="icon">
-              <div className="insta-icon-bg">
-                <a
-                  href="https://www.instagram.com/sayan.kar2000/"
-                  className="insta-icon"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FaInstagram />
-                </a>
-              </div>
             </div>
           </div>
         </div>
